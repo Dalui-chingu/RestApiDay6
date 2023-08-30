b0e56fe96b5c81b6340efe502c67a1f52733566e
Dalui-chingu <125961514+Dalui-chingu@users.noreply.github.com>
2023-08-30 11:56:58 +0530

    Add files via upload

---
+++ Day6cc/src/main/java/com/example/demo/Day6ccApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day6ccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day6ccApplication.class, args);
+	}
+
+}
